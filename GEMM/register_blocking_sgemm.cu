6700f5d88c60072f19bcf6fd83b2f48ce3856b45
CC-C122 <2513326504@qq.com>
2026-05-14 11:33:30 +0800

    完成寄存器分块GEMM

--- GEMM/register_blocking_sgemm.cu
+++ GEMM/register_blocking_sgemm.cu
@@ -9,10 +9,10 @@
 #define BLOCK_X (C_TILE / THREAD_TILE_X)
 
 __global__
-void matmul_reg_block_2x2_kernel(const float* A, const float* B, float C, int M, int N, int K){
+void matmul_reg_block_2x2_kernel(const float* A, const float* B, float* C, int M, int N, int K){
     //发射的block为8x8
-    __shared__ float* As[C_TILE][K_TILE];
-    __shared__ float* Bs[K_TILE][C_TILE];
+    __shared__ float As[C_TILE][K_TILE];
+    __shared__ float Bs[K_TILE][C_TILE];
 
     int tx = threadIdx.x;
     int ty = threadIdx.y;
@@ -46,7 +46,7 @@ void matmul_reg_block_2x2_kernel(const float* A, const float* B, float C, int M,
 
     int num_tiles = (K + K_TILE - 1) / K_TILE;
 
-    for(int title = 0; title < num_tiles; ++title){
+    for(int tile = 0; tile < num_tiles; ++tile){
         //一个 As 256个元素
         //总共只有64个线程，一个线程要加载4个A，4个B
         for(int load = 0; load < 4; load++){
@@ -56,16 +56,55 @@ void matmul_reg_block_2x2_kernel(const float* A, const float* B, float C, int M,
             int a_local_col = idx % K_TILE;
 
             int a_global_row = block_row + a_global_row;
-            int a_global_col = block_col + a_global_col;
+            int a_global_col = tile*K_TILE + a_global_col;
 
             if(a_global_row < M && a_global_col < K){
-                As[a_local_row][a_local_col] = A[a_global_row * k + a_global_col];
+                As[a_local_row][a_local_col] = A[a_global_row * K + a_global_col];
             }else{
                 As[a_global_row][a_global_col] = 0.0f;
             }
 
             int b_local_row = idx / C_TILE;
             int b_local_col = idx % C_TILE;
+
+            int b_global_row = tile*K_TILE + b_local_row;
+            int b_global_col = block_col + b_local_col;
+
+            if(b_global_row < K && b_global_col < N){
+                Bs[b_global_row][b_local_col] = B[b_global_row*N + b_global_col];
+            }else{
+                Bs[b_global_row][b_global_col] = 0.0f;
+            }
         }
+
+        __syncthreads();
+
+        #pragma unroll
+        for(int i = 0; i < K_TILE; ++i){
+            float a0 = As[local_row0][i];
+            float a1 = As[local_row1][i];
+
+            float b0 = Bs[i][local_row0];
+            float b1 = Bs[i][local_col1];
+
+            sum00 += a0 * b0;
+            sum01 += a0 * b1;
+            sum10 += a1 * b0;
+            sum11 += a1 * b1;
+        }
+    }
+    __syncthreads();
+
+    if(row0 < M && col0 < N){
+        C[row0*N + col0] = sum00;
+    }
+    if(row0 < M && col1 < N){
+        C[row0*N + col1] = sum01;
+    }
+    if(row1 < M && col0 < N){
+        C[row1 *N + col0] = sum10;
+    }
+    if(row1 < M && col1 < N){
+        C[row1*N + col1] = sum11;
     }
 }
